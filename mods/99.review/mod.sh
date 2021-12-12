5dec285bd338c37c6d631e14225dda820eaf6d1d
Zhenkai Weng <theoaktree1040@gmail.com>
2021-12-11 21:18:26 -0800

    Fix file not found

--- mods/99.review/mod.sh
+++ mods/99.review/mod.sh
@@ -1,5 +1,9 @@
-echo -en "$red"
-cat "$ERRLOG"
-echo -e "$purple"
-cat "$DATA/todo"
+if [ -f "$ERRLOG" ]; then
+    echo -en "$red"
+    cat "$ERRLOG"
+fi
+if [ -f "$DATA/todo" ]; then
+    echo -e "$purple"
+    cat "$DATA/todo"
+fi
 echo -en "$reset"
