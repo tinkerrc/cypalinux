796d3856c8e6b885f6ca1c4c32b2dc0bf0e748c9
Zhenkai Weng <theoaktree1040@gmail.com>
2021-12-11 22:28:18 -0800

    Improve manual-pkgs

--- mods/xx.manual-pkgs/mod.sh
+++ mods/xx.manual-pkgs/mod.sh
@@ -1,1 +1,6 @@
-comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort -u) | tee "$BASE/manually-installed"
+pkglist=/var/log/installer/initial-status.gz
+if [ -f "$RC/$OS/initial-status.gz" ]; then
+    pkglist="$RC/$OS/initial-status.gz"
+fi
+comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc $pkglist | sed -n 's/^Package: //p' | sort -u) | tee "$DATA/manually-installed"
+psuccess "Nonpreinstalled packages have been stored into \$DATA/manually-installed"
