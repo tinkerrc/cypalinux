2321b5d84c211da79a24ab11c376e9efa75594bc
Zhenkai Weng <theoaktree1040@gmail.com>
2021-12-11 20:01:04 -0800

    Fix networking

--- mods/11.netsec/mod.sh
+++ mods/11.netsec/mod.sh
@@ -2,8 +2,12 @@ instconf $RC/interfaces /etc/network/interfaces
 instconf $RC/nsswitch.conf /etc/nsswitch.conf
 instconf $RC/host.conf /etc/host.conf
 instconf $RC/hosts /etc/hosts
+instconf $RC/resolved.conf /etc/systemd/resolved.conf
+rm -rf /etc/systemd/resolved.conf.d
 echo "127.0.0.1 localhost $(hostname)" >> /etc/hosts
 psuccess "Resolver configurations installed"
+
+systemctl restart systemd-resolved && psuccess "systemd-resolved restarted" || perror "Failed to restart systemd-resolved"
 
 rm -f /home/*/.netrc
 rm -f /home/*/.forward
