18a345615fb052c50614a73d4d5f2523461bf548
Zhenkai Weng <theoaktree1040@gmail.com>
2021-12-11 20:42:33 -0800

    Fix LAMP

--- mods/50.apache/use.sh
+++ mods/50.apache/use.sh
@@ -4,8 +4,8 @@ instconf $RC/security.conf /etc/apache2/conf-available/security.conf
 instconf $RC/modsecurity.conf /etc/modsecurity/modsecurity.conf
 instconf $RC/crs-setup.conf /usr/share/modsecurity-crs/crs-setup.conf
 instconf $RC/security2.conf /etc/apache2/mods-available/security2.conf
-# TEST: see if it moves files properly
-instdir $RC/rules /etc/modsecurity/rules
+# NOTE: /usr/share/modsecurity-crs/ contains the same thing
+# instdir $RC/rules /etc/modsecurity/rules
 
 mkdir -p /opt/modsecurity/var/log/
 touch /opt/modsecurity/var/log/debug.log
